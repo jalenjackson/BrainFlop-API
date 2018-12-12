9faba8e9e2cb006f661f3fd0497773ede2bb4676
Jalen Jackson <jalenjack201@hotmail.com>
2018-12-12 03:35:45 -0500

    c

--- api/controllers/tags/getAllTags.js
+++ api/controllers/tags/getAllTags.js
@@ -10,11 +10,12 @@ exports.getAllTags = (req, res) => {
       const response = {
         count: tags.length,
         tags: tags.map((tag) => {
-          console.log(tag)
+          console.log(tag.image)
           return {
             _id: tag._id,
             name: tag.name,
             count: tag.count,
+            image: tag.image
           };
         }),
       };
